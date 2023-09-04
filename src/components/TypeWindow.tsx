c4b3b4e54aff33e99a8dbb33a5790a26e49994f6
TomaszLaboj <tomasz.laboj@gmail.com>
2023-09-04 13:46:35 +0100

    with use Effect

--- src/components/TypeWindow.tsx
+++ src/components/TypeWindow.tsx
@@ -6,17 +6,29 @@ export function TypeWindow(): JSX.Element {
     const paragraphsArray:string[] = paragraphs;
     const [originalText, setOriginalText] = useState<string>(paragraphsArray[0]);
     const [typedText, setTypedText] = useState<string>('');
+    const [inError, setInError] = useState<boolean>(false);
     
     
     
+    useEffect (()=> {
+        if(originalText.slice(0,typedText.length+1) !== typedText) {
+            setInError(true);
+        }else{
+            setInError(false);
+        }
+        if(originalText === typedText) {
+            alert('well done! Try next challenge')
+            setOriginalText(paragraphsArray[1])
+        }
+    }, [typedText, originalText, paragraphsArray])
     console.log(typedText)
 
     return (
     <div>
         <p className="original-text">{originalText}</p><br/>
         Type here:
-        <textarea  className="typed-text" onChange={(event) => {setTypedText(event.target.value)}}/>
-       
+        <textarea  className="typed-text-not-error" onChange={(event) => {setTypedText(event.target.value)}}/>
+       <p>{inError}</p>
     </div>
     );
 }
