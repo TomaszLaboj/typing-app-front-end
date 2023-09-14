ce59657403c22342e002a28ff887fc52e6038d79
TomaszLaboj <tomasz.laboj@gmail.com>
2023-09-14 21:15:31 +0100

    change texts json , delete old version

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,8 @@
-import { TypeWindow} from "./TypeWindow";
+import { TypeWindow } from "./TypeWindow";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <TypeWindow />
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
