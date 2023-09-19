85685f08c8455eaa8e0871558e58c92e77f5113d
TomaszLaboj <tomasz.laboj@gmail.com>
2023-09-19 18:42:33 +0100

    new version

--- src/components/TypeWindow.tsx
+++ src/components/TypeWindow.tsx
@@ -1,34 +1,27 @@
 import { useState, useEffect } from "react";
-import { paragraphs } from "./paragraphs.json"
-import  "../styles.css"
+import "../styles.css";
+import { faker } from "@faker-js/faker";
 
 export function TypeWindow(): JSX.Element {
-    const paragraphsArray:string[] = paragraphs;
-    const [originalText, setOriginalText] = useState<string>(paragraphsArray[0]);
-    const [typedText, setTypedText] = useState<string>('');
-    const [inError, setInError] = useState<boolean>(false);
-    
-    
-    
-    useEffect (()=> {
-        if(originalText.slice(0,typedText.length+1) !== typedText) {
-            setInError(true);
-        }else{
-            setInError(false);
-        }
-        if(originalText === typedText) {
-            alert('well done! Try next challenge')
-            setOriginalText(paragraphsArray[1])
-        }
-    }, [typedText, originalText, paragraphsArray])
-    console.log(typedText)
+    const [singleWord, setSingleWord] = useState<string>("");
+    const [typedText, setTypedText] = useState<string>("");
 
+    // const [inError, setInError] = useState<boolean>(false);
+
+    useEffect(() => {
+        generateNewWord();
+    }, []);
+
+    const generateNewWord = () => {
+        const newWord = faker.word.sample();
+        setSingleWord(newWord);
+    };
+    
     return (
-    <div>
-        <p className="original-text">{originalText}</p><br/>
-        Type here:
-        <textarea  className="typed-text-not-error" onChange={(event) => {setTypedText(event.target.value)}}/>
-       <p>{inError}</p>
-    </div>
+        <div>
+            <div onKeyDown={(key)=> key.}>{typedText}</div>
+            <div className="original-text">{singleWord}</div>
+            <button onClick={generateNewWord}>Generate</button>
+        </div>
     );
 }
